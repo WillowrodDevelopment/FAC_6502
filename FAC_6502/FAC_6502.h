c3a22309fba5eec96c80a7e72ecfee3f92f2839c
mikehallCD <mike@chipmunkdata.co.uk>
2025-03-07 09:14:03 +0000

    Initial Commit

---
+++ FAC_6502/FAC_6502.h
@@ -0,0 +1,18 @@
+//
+//  FAC_6502.h
+//  FAC_6502
+//
+//  Created by Mike Hall on 07/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FAC_6502.
+FOUNDATION_EXPORT double FAC_6502VersionNumber;
+
+//! Project version string for FAC_6502.
+FOUNDATION_EXPORT const unsigned char FAC_6502VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FAC_6502/PublicHeader.h>
+
+
---
+++ FAC_6502Tests/FAC_6502Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FAC_6502Tests.swift
+//  FAC_6502Tests
+//
+//  Created by Mike Hall on 07/03/2025.
+//
+
+import XCTest
+@testable import FAC_6502
+
+final class FAC_6502Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
